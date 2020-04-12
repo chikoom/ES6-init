d9627897eeeca36b1adf90ea731a8a5b662e132e
Chikoom <callforcopy@gmail.com>
2020-04-12 14:31:46 +0300

    updated webpack.config and installed babel core

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+var webpack = require('webpack');
+
+module.exports = {
+	entry: './script.js',
+	output: {filename: 'bundle.js'},
+	mode: 'development',
+	module: {
+		rules: [
+			{test: /\.js?/, loader: 'babel-loader', exclude: /node_modules/}
+		]
+	}
+}
