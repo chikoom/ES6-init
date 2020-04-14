ba3fd8baa26797aff1f55b1b8a440535ca8a3689
Chikoom <callforcopy@gmail.com>
2020-04-14 11:22:40 +0300

    added arrow functions and destructuring

--- script.js
+++ script.js
@@ -46,6 +46,7 @@ templateStringExample("Chikoom", 100);
 
 
 
+// SET Exmaple
 var books = new Set();
 books.add('Pride and Prejudice');
 books.add('War and Peace')
@@ -66,61 +67,163 @@ console.log('set.size', set.size);
 
 
 
-
+// MAP Exmaple
 var course = new Map();
-		course.set('react', {description: 'ui'});
-		course.set('jest', {description: 'testing'});
+course.set('react', {description: 'ui'});
+course.set('jest', {description: 'testing'});
 
-		console.log(course);
-		console.log(course.react);
-		console.log(course.get('react'));
+console.log(course);
+console.log(course.react);
+console.log(course.get('react'));
 
-		var details = new Map([
-			[new Date(), 'today'],
-			['items', [1, 2]]
-		]);
+var details = new Map([
+	[new Date(), 'today'],
+	['items', [1, 2]]
+]);
 
-		console.log(details.size);
+console.log(details.size);
 
-		details.forEach(function(item) {
-			console.log(item);
-		});
+details.forEach(function(item) {
+	console.log(item);
+});
 
 
 
 var cats = ["Tabby", "Siamese", "Persian"];
-		var dogs = ["Golden Retriever", "Pug", "Schnauzer"];
+var dogs = ["Golden Retriever", "Pug", "Schnauzer"];
 
-		var animals = ["Whale", "Giraffe", ...cats, "Snake", ...dogs, "Coyote"];
+var animals = ["Whale", "Giraffe", ...cats, "Snake", ...dogs, "Coyote"];
 
-		console.log(animals);
+console.log(animals);
 
 
-
+// For-Of Exmaple
 for (let letter of 'JavaScript') {
 			console.log(letter);
-		}
+}
 
-		var topics = ['JavaScript', 'Node', 'React'];
+var topics = ['JavaScript', 'Node', 'React'];
 
-		for (let topic of topics) {
-			console.log(topic);
-		}
+for (let topic of topics) {
+	console.log(topic);
+}
 
-		var topics = new Map();
-		topics.set('HTML', '/class/html');
-		topics.set('CSS', '/class/css');
-		topics.set('JavaScript', '/class/javascript');
-		topics.set('Node', '/class/node');
+var topics = new Map();
+topics.set('HTML', '/class/html');
+topics.set('CSS', '/class/css');
+topics.set('JavaScript', '/class/javascript');
+topics.set('Node', '/class/node');
 
-		for (let topic of topics.keys()) {
-			console.log(topic, "is the course name");
-		}
+for (let topic of topics.keys()) {
+	console.log(topic, "is the course name");
+}
 
-		for (let topic of topics.values()) {
-			console.log("The course description can be found at", topic);
-		}
+for (let topic of topics.values()) {
+	console.log("The course description can be found at", topic);
+}
 
-		for (let course of topics.entries()) {
-			console.log(course);
-		}
+for (let course of topics.entries()) {
+	console.log(course);
+}
+
+
+function addNumbers(x=0, y=0){
+	console.log(x+y);
+}
+
+addNumbers();
+addNumbers(2,3);
+
+
+// Objects Literals 0 base Array
+
+var cat = {
+	speak: function(times, sound){
+		console.log(Array(times+1).join(sound));
+	},
+	purr: function(times){
+		console.log(Array(times+1).join("PURR "));
+	}
+
+};
+
+cat.speak(3,"meow");
+cat.purr(5);
+
+// Objects Literals ES6
+
+var dog = {
+	speak(times, sound){
+		console.log(sound.repeat(times));
+	},
+	woof(times){
+		console.log("woof".repeat(times));
+	}
+
+};
+
+dog.speak(3,"bark");
+dog.woof(5);
+
+// Arrow functions
+
+var studentList = students => console.log(students);
+studentList(["Idan", "Zohar", "Kundi"]);
+
+var studentListNumber = (school, students) => console.log(`the students ${students} are in ${school} school`);
+studentListNumber("borochov", ["Idan", "Zohar", "Kundi"]);
+
+
+// Arrow and this scope
+
+var kundi = {
+	name:"Kundofon",
+	actions:["Eat", "Sleep", "Jump"],
+	printActions: function(){
+		this.actions.forEach(function(action){
+			var str = this.name+" likes to "+action;
+			console.log(str);
+		}.bind(this));
+	}
+};
+
+kundi.printActions();
+
+// ES6 version
+
+var kundofon = {
+	name:"Kundofoni",
+	sounds:["Meow", "Purr", "Grr"],
+	printSounds(){
+		this.sounds.forEach(sound => {
+			var str = this.name+" likes to "+sound;
+			console.log(str);
+		});
+	}
+};
+
+kundofon.printSounds();
+
+
+
+// Destructuring
+
+var [first,,,foutrh] = ["Tel Aviv", "Ramat gan", "Givataim", "Nahariya", "Haifa"];
+console.log(first,foutrh);
+
+// Destructuring 2
+
+var vecation = {
+	destination: "Amsterdam",
+	days: 2,
+	activity: "walking",
+	price: 5000
+}
+
+function vecationMarketing({destination, days}) {
+	return `Come to ${destination} for ${days} days!`;
+}
+
+console.log(vecationMarketing(vecation));
+
+// Generators
