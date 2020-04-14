a401f60b682c5f943be121fe02d3e433f3d23574
Chikoom <callforcopy@gmail.com>
2020-04-14 10:29:44 +0300

    Added Spread examples

--- script.js
+++ script.js
@@ -85,3 +85,12 @@ var course = new Map();
 		details.forEach(function(item) {
 			console.log(item);
 		});
+
+
+
+var cats = ["Tabby", "Siamese", "Persian"];
+		var dogs = ["Golden Retriever", "Pug", "Schnauzer"];
+
+		var animals = ["Whale", "Giraffe", ...cats, "Snake", ...dogs, "Coyote"];
+
+		console.log(animals);
