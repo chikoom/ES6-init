96d15de48bb193f22b40b994340045200c618d0b
Chikoom <callforcopy@gmail.com>
2020-04-14 10:28:25 +0300

    Added SET examples

--- dist/bundle.js
+++ dist/bundle.js
@@ -93,7 +93,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("var nameBuilder = function (firstName = \"Joe\", lastName = \"Doe\") {\n  console.log(firstName + \" \" + lastName);\n};\n\nnameBuilder();\n\n//# sourceURL=webpack:///./script.js?");
+eval("// Default veriables in functions + template strings - new to ES6\nvar nameBuilder = function (firstName = \"Joe\", lastName = \"Doe\") {\n  console.log(`${firstName} ${lastName}`);\n};\n\nnameBuilder(); // const is a constant veriable new in ES6\n\nconst numberOfDivs = 18; // let is a scope veriable new in ES6\n\nfor (let i = 0; i <= numberOfDivs; i++) {\n  var divi = document.createElement('div');\n  divi.innerHTML = i;\n  divi.addEventListener('click', function (e) {\n    alert(\"box number \" + i);\n  }, false);\n  document.getElementsByTagName('section')[0].appendChild(divi);\n}\n\nfunction templateStringExample(name = \"No name entered\", value = 0) {\n  let addedValue = 50;\n  let stringOutput = `\n\t\tThe name entered is: ${name}\\n\n\t\tThe value entered is: ${value}\\n\n\t\tThe added value is: ${addedValue}\\n\n\t\tThe total value is: ${value + addedValue}\\n\n\t`;\n  var textP = document.createElement('p');\n  textP.innerHTML = stringOutput;\n  document.getElementById('templateString').appendChild(textP);\n}\n\ntemplateStringExample(\"Chikoom\", 100);\n\n//# sourceURL=webpack:///./script.js?");
 
 /***/ })
 
--- script.js
+++ script.js
@@ -1,5 +1,66 @@
+
+
+// Default veriables in functions + template strings - new to ES6
 var nameBuilder = function(firstName="Joe", lastName="Doe") {
-			console.log(firstName + " " + lastName);
+			console.log(`${firstName} ${lastName}`);
 		};
 
 nameBuilder();
+
+// const is a constant veriable new in ES6
+const numberOfDivs = 18;
+
+// let is a scope veriable new in ES6
+for (let i = 0; i <= numberOfDivs; i++) 
+{
+	var divi = document.createElement('div');
+	divi.innerHTML = i;
+
+	divi.addEventListener('click', function(e) {
+	 alert("box number "+i);
+	}, false);
+
+	document.getElementsByTagName('section')[0].appendChild(divi);
+}
+
+
+function templateStringExample(name="No name entered", value=0)
+{
+	let addedValue = 50;
+	let stringOutput = `
+		The name entered is: ${name}\n
+		The value entered is: ${value}\n
+		The added value is: ${addedValue}\n
+		The total value is: ${value+addedValue}\n
+	`;
+
+	var textP = document.createElement('p');
+	textP.innerHTML = stringOutput;
+
+	
+	document.getElementById('templateString').appendChild(textP);
+
+}
+
+templateStringExample("Chikoom", 100);
+
+
+
+var books = new Set();
+books.add('Pride and Prejudice');
+books.add('War and Peace')
+	 .add('Oliver Twist');
+
+console.log(books);
+console.log('how many books?', books.size);
+console.log('has Oliver Twist?', books.has('Oliver Twist'));
+books.delete('Oliver Twist');
+console.log('has Oliver Twist still?', books.has('Oliver Twist'));
+
+
+
+var data = [4,2,4,4,2,5,1,6,7,5,6,8,2,7];
+var set = new Set(data);
+console.log('data.length', data.length);
+console.log('set.size', set.size);
+
