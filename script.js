0d83674c145c1ed718d75401674ee257899fdb71
Chikoom <callforcopy@gmail.com>
2020-04-14 10:28:59 +0300

    Added MAP examples

--- script.js
+++ script.js
@@ -64,3 +64,24 @@ var set = new Set(data);
 console.log('data.length', data.length);
 console.log('set.size', set.size);
 
+
+
+
+var course = new Map();
+		course.set('react', {description: 'ui'});
+		course.set('jest', {description: 'testing'});
+
+		console.log(course);
+		console.log(course.react);
+		console.log(course.get('react'));
+
+		var details = new Map([
+			[new Date(), 'today'],
+			['items', [1, 2]]
+		]);
+
+		console.log(details.size);
+
+		details.forEach(function(item) {
+			console.log(item);
+		});
